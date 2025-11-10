97c2b7a74b13908e49cb49e03627834340ab96d8
Ali Can Doğru <ali.can.dgru10@gmail.com>
2025-11-10 16:37:24 +0300

    vercel config added

--- backend/src/server.js
+++ backend/src/server.js
@@ -32,12 +32,14 @@ app.use((err, req, res, next) => {
 const startServer = async () => {
     try {
         await connectDB()
-        app.listen(ENV.PORT, () => {
-            console.log(`Server is running on port ${ENV.PORT}`);
-        })
+        if (ENV.NODE_ENV !=="production") {
+            app.listen(ENV.PORT, () => {console.log(`Server is running on port ${ENV.PORT}`);})
+        }
     } catch (error) {
         console.error("Failed to start server:", error);
         process.exit(1);
     }                               
 }
 startServer();
+
+export default app;
